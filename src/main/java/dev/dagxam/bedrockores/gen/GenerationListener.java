36b86f239200b6ec3425275bf1f0e23bf153960c
Maxdag <dagxam@gmail.com>
2025-10-23 12:30:56 +0300

    Update GenerationListener.java

--- src/main/java/dev/dagxam/bedrockores/gen/GenerationListener.java
+++ src/main/java/dev/dagxam/bedrockores/gen/GenerationListener.java
@@ -9,7 +9,6 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkLoadEvent;
-import org.bukkit.util.BoundingBox;
 import org.bukkit.plugin.Plugin;
 
 import java.util.*;
