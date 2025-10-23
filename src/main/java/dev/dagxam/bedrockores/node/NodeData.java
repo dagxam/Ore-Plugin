1ccc0bb9eeaa2b9b1fc808babb89c43f5b01d7ff
Maxdag <dagxam@gmail.com>
2025-10-23 12:01:46 +0300

    Update NodeData.java

--- src/main/java/dev/dagxam/bedrockores/node/NodeData.java
+++ src/main/java/dev/dagxam/bedrockores/node/NodeData.java
@@ -1,1 +1,19 @@
+package dev.dagxam.bedrockores.node;
 
+import org.bukkit.Material;
+
+public class NodeData {
+    public final Material oreMaterial; // тип руды (DEEPSLATE_*_ORE)
+    public int hitsRemaining;
+    public final int maxHits;
+
+    public NodeData(Material oreMaterial, int hitsRemaining, int maxHits) {
+        this.oreMaterial = oreMaterial;
+        this.hitsRemaining = hitsRemaining;
+        this.maxHits = maxHits;
+    }
+
+    public float progress() {
+        return 1.0f - (hitsRemaining / (float) maxHits);
+    }
+}
