8dc56de86aa3dc3fdefeed9ca8c8aded361312ef
Maxdag <dagxam@gmail.com>
2025-10-23 12:01:15 +0300

    Update BedrockOresPlugin.java

--- src/main/java/dev/dagxam/bedrockores/BedrockOresPlugin.java
+++ src/main/java/dev/dagxam/bedrockores/BedrockOresPlugin.java
@@ -1,1 +1,41 @@
+package dev.dagxam.bedrockores;
 
+import dev.example.bedrockores.gen.GenerationListener;
+import dev.example.bedrockores.node.NodeManager;
+import dev.example.bedrockores.node.OreListeners;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BedrockOresPlugin extends JavaPlugin {
+
+    private NodeManager nodeManager;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        this.nodeManager = new NodeManager(this);
+        nodeManager.load();
+
+        // Листенеры
+        Bukkit.getPluginManager().registerEvents(new GenerationListener(this, nodeManager), this);
+        Bukkit.getPluginManager().registerEvents(new OreListeners(this, nodeManager), this);
+
+        // Периодическое сохранение
+        Bukkit.getScheduler().runTaskTimer(this, nodeManager::save, 20L * 60L, 20L * 60L); // каждую минуту
+
+        getLogger().info("BedrockOres enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        try {
+            nodeManager.save();
+        } catch (Exception e) {
+            getLogger().severe("Failed to save nodes: " + e.getMessage());
+        }
+    }
+
+    public NodeManager getNodeManager() {
+        return nodeManager;
+    }
+}
